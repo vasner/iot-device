054bf7fbafa8e9b84ca057f587f50d8794d28c74
Vasiliy Nerozin <vnerozin@gmail.com>
2023-12-01 16:29:27 +0300

    Add measure and default methods; Join sensor in structure

--- host/main.c
+++ host/main.c
@@ -4,16 +4,35 @@
 #include <stdint.h>
 #include <stdio.h>
 
-float temperature = 0;
-uint8_t humidity = 99;
-uint16_t pressure = 700;
-uint8_t relay = 0xF;
-uint8_t led[3] = {0, 0, 0};
+#define NUM_RELAYS (4)
+#define NUM_LEDS (3)
+
+uint8_t relay[NUM_RELAYS] = {0};
+uint8_t led[NUM_LEDS] = {0};
+
+typedef struct {
+    float temperature;
+    uint8_t humidity;
+    uint16_t pressure;
+} sensors_t;
+
+void sensors_measure(sensors_t* p_s) {
+    p_s->temperature = 25.0f;
+    p_s->humidity = 33;
+    p_s->pressure = 768;
+}
+
+void sensors_default(sensors_t* p_s) {
+    p_s->temperature = 23.0f;
+    p_s->humidity = 60;
+    p_s->pressure = 700;
+}
 
 int main(void) {
-    int8_t b = -1;           // 0xFF
-    int16_t a = (int16_t)b;  // 0xFFFF
-    uint8_t a6 = (uint8_t)pressure;
-    printf("%4x\n", a);
+    sensors_t s;
+    sensors_default(&s);
+    printf("%f, %d, %d\n", s.temperature, s.pressure, s.humidity);
+    sensors_measure(&s);
+    printf("%f, %d, %d\n", s.temperature, s.pressure, s.humidity);
     return 0;
 }
