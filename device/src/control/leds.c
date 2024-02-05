86ba96c2a694332d686d31c8464d61c535b22fdb
Vasiliy Nerozin <vnerozin@gmail.com>
2024-02-05 22:33:20 +0300

    Hardware blinking mode for all leds

--- device/src/control/leds.c
+++ device/src/control/leds.c
@@ -8,21 +8,42 @@
 
 #include "platform.h"
 
+/**
+ * Set timer 4 period to 500ms.
+ * `-1` is explained in
+ * [Reference Manual page page 332](https://www.arterychip.com/download/RM/RM_AT32F435_437_EN_V2.05.pdf#page=332)
+ */
+#define _LED_TIMER  (TMR4)
+#define _TMR_PERIOD_CLOCK (5000 - 1)
+#define _TMR_FREQ_DIV (PLATFORM_CRM_TMR_FREQ_HZ / 10000 - 1)
+
 #define _LED_RED_PIN (GPIO_PINS_13)
 #define _LED_RED_GPIO (GPIOD)
 #define _LED_RED_GPIO_CRM_CLK (CRM_GPIOD_PERIPH_CLOCK)
+#define _LED_RED_TMR_CHAN (TMR_SELECT_CHANNEL_2)
+#define _LED_RED_SOURCE_TYPE (GPIO_PINS_SOURCE13)
+#define _LED_RED_MUX_SEL (GPIO_MUX_2)
 
 #define _LED_AMBER_PIN (GPIO_PINS_14)
 #define _LED_AMBER_GPIO (GPIOD)
 #define _LED_AMBER_GPIO_CRM_CLK (CRM_GPIOD_PERIPH_CLOCK)
+#define _LED_AMBER_TMR_CHAN (TMR_SELECT_CHANNEL_3)
+#define _LED_AMBER_SOURCE_TYPE (GPIO_PINS_SOURCE14)
+#define _LED_AMBER_MUX_SEL (GPIO_MUX_2)
 
 #define _LED_GREEN_PIN (GPIO_PINS_15)
 #define _LED_GREEN_GPIO (GPIOD)
-#define _LED_GPIO_CRM_CLK (CRM_GPIOD_PERIPH_CLOCK)
+#define _LED_GREEN_GPIO_CRM_CLK (CRM_GPIOD_PERIPH_CLOCK)
+#define _LED_GREEN_TMR_CHAN (TMR_SELECT_CHANNEL_4)
+#define _LED_GREEN_SOURCE_TYPE (GPIO_PINS_SOURCE15)
+#define _LED_GREEN_MUX_SEL (GPIO_MUX_2)
 
-uint16_t _pins[NUM_LEDS] = {_LED_RED_PIN, _LED_AMBER_PIN, _LED_GREEN_PIN};
-gpio_type* _gpios[NUM_LEDS] = {_LED_RED_GPIO, _LED_AMBER_GPIO, _LED_GREEN_GPIO};
-crm_periph_clock_type _crm_clks[NUM_LEDS] = {_LED_RED_GPIO_CRM_CLK, _LED_AMBER_GPIO_CRM_CLK, _LED_GPIO_CRM_CLK};
+static uint16_t _pins[NUM_LEDS] = {_LED_RED_PIN, _LED_AMBER_PIN, _LED_GREEN_PIN};
+static gpio_type* _gpios[NUM_LEDS] = {_LED_RED_GPIO, _LED_AMBER_GPIO, _LED_GREEN_GPIO};
+static tmr_channel_select_type _tmr_chans[NUM_LEDS] = {_LED_RED_TMR_CHAN, _LED_AMBER_TMR_CHAN, _LED_GREEN_TMR_CHAN};
+static gpio_mux_sel_type _mux_sels[NUM_LEDS] = {_LED_RED_MUX_SEL, _LED_AMBER_MUX_SEL, _LED_GREEN_MUX_SEL};
+static crm_periph_clock_type _crm_clks[NUM_LEDS] = {_LED_RED_GPIO_CRM_CLK, _LED_AMBER_GPIO_CRM_CLK, _LED_GREEN_GPIO_CRM_CLK,};
+static gpio_pins_source_type _source_types[NUM_LEDS] = {_LED_RED_SOURCE_TYPE, _LED_AMBER_SOURCE_TYPE, _LED_GREEN_SOURCE_TYPE,};
 
 void _led_init(led_type_t led) {
     gpio_init_type gpio_init_struct;
@@ -30,39 +51,71 @@ void _led_init(led_type_t led) {
     crm_periph_clock_enable(_crm_clks[led], TRUE);
     gpio_default_para_init(&gpio_init_struct);
 
+    gpio_init_struct.gpio_pins = _pins[led];
     gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
     gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
-    gpio_init_struct.gpio_mode = GPIO_MODE_OUTPUT;
-    gpio_init_struct.gpio_pins = _pins[led];
     gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
+    gpio_init_struct.gpio_mode = GPIO_MODE_OUTPUT;
     gpio_init(_gpios[led], &gpio_init_struct);
 }
 
+void _led_blink_enable(led_type_t led) {
+    gpio_init_type gpio_init_struct;
+    tmr_output_config_type tmr_output_struct;
+
+    gpio_default_para_init(&gpio_init_struct);
+    gpio_init_struct.gpio_pins = _pins[led];
+    gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
+    gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
+    gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
+    gpio_init_struct.gpio_mode = GPIO_MODE_MUX;
+    gpio_init(_gpios[led], &gpio_init_struct);
+    gpio_pin_mux_config(_gpios[led], _source_types[led], _mux_sels[led]);
+
+    tmr_output_default_para_init(&tmr_output_struct);
+    tmr_output_struct.oc_mode = TMR_OUTPUT_CONTROL_SWITCH;
+    tmr_output_struct.oc_polarity = TMR_OUTPUT_ACTIVE_LOW;
+    tmr_output_struct.oc_output_state = TRUE;
+    tmr_output_channel_config(_LED_TIMER, _tmr_chans[led], &tmr_output_struct);
+    tmr_channel_value_set(_LED_TIMER, _tmr_chans[led], _TMR_PERIOD_CLOCK);
+}
+
 void leds_init(leds_t* state) {
+    crm_periph_clock_enable(CRM_TMR4_PERIPH_CLOCK, TRUE);
+    
+    tmr_base_init(_LED_TIMER, _TMR_PERIOD_CLOCK, _TMR_FREQ_DIV);
+    tmr_cnt_dir_set(_LED_TIMER, TMR_COUNT_UP);
+    tmr_clock_source_div_set(_LED_TIMER, TMR_CLOCK_DIV1);
+    tmr_overflow_event_disable(_LED_TIMER, TRUE);
+    
     for (int led = 0; led < NUM_LEDS; led++) {
-        _led_init(led);
         leds_off(state, led);
     }
+
+    tmr_counter_enable(_LED_TIMER, TRUE);
 }
 
 void leds_on(leds_t* state, led_type_t led) {
+    _led_init(led);
     state->led_state[led] = LED_STATE_ON;
     _gpios[led]->clr = _pins[led];
 }
 
 void leds_off(leds_t* state, led_type_t led) {
+    _led_init(led);
     state->led_state[led] = LED_STATE_OFF;
     _gpios[led]->scr = _pins[led];
 }
 
 void leds_toggle(leds_t* state, led_type_t led) {
     if (state->led_state[led] == LED_STATE_BLINK) return;
+    _led_init(led);
     state->led_state[led] = (state->led_state[led] == LED_STATE_ON) ? LED_STATE_OFF : LED_STATE_ON;
     _gpios[led]->odt ^= _pins[led];
 }
 
 void leds_blink(leds_t* state, led_type_t led) {
-    // TODO: Implement
+    _led_blink_enable(led);
     state->led_state[led] = LED_STATE_BLINK;
 }
 
--- device/src/main.c
+++ device/src/main.c
@@ -5,118 +5,29 @@
 #include <string.h>
 
 #include "platform.h"
-#include "usb_conf.h"
 #include "vcom.h"
 #include "vcom_console.h"
 #include "utils.h"
 #include "leds.h"
 
-/**
- * Set timer 1 period to 50ms.
- * Set timer 4 period to 500ms.
- * `-1` is explained in
- * [Reference Manual page page 332](https://www.arterychip.com/download/RM/RM_AT32F435_437_EN_V2.05.pdf#page=332)
- */
-#define TMR1_PERIOD_CLOCK (500 - 1)
-#define TMR1_FREQ_DIV (PLATFORM_CRM_TMR_FREQ_HZ / 10000 - 1)
-
-#define TMR4_PERIOD_CLOCK (5000 - 1)
-#define TMR4_FREQ_DIV (PLATFORM_CRM_TMR_FREQ_HZ / 10000 - 1)
-
 leds_t leds;
 
 int main(void) {
-    gpio_init_type gpio_init_struct;
-    tmr_output_config_type tmr_output_struct;
-
     platform_init_system_clock();
     delay_init();
     leds_init(&leds);
+    
+    leds_on(&leds, LED_AMBER);
+    
     vcom_init();
-
-    leds_on(&leds, LED_GREEN);
-
-    nvic_priority_group_config(NVIC_PRIORITY_GROUP_1);
-    nvic_irq_enable(TMR1_CH_IRQn, 1, 0);
-    nvic_irq_enable(TMR4_GLOBAL_IRQn, 1, 0);
-
-    crm_periph_clock_enable(CRM_TMR1_PERIPH_CLOCK, TRUE);
-    crm_periph_clock_enable(CRM_TMR4_PERIPH_CLOCK, TRUE);
-    crm_periph_clock_enable(CRM_GPIOB_PERIPH_CLOCK, TRUE);
-    crm_periph_clock_enable(CRM_GPIOD_PERIPH_CLOCK, TRUE);
-
-    // Configure LED3 as timer 4 channel 3 output
-    gpio_default_para_init(&gpio_init_struct);
-    gpio_init_struct.gpio_pins = GPIO_PINS_14;
-    gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
-    gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
-    gpio_init_struct.gpio_mode = GPIO_MODE_MUX;
-    gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
-    gpio_init(GPIOD, &gpio_init_struct);
-    gpio_pin_mux_config(GPIOD, GPIO_PINS_SOURCE14, GPIO_MUX_2);
-
-    tmr_base_init(TMR4, TMR4_PERIOD_CLOCK, TMR4_FREQ_DIV);
-    tmr_cnt_dir_set(TMR4, TMR_COUNT_UP);
-    tmr_clock_source_div_set(TMR4, TMR_CLOCK_DIV1);
-
-    tmr_overflow_event_disable(TMR4, TRUE);
-    tmr_output_default_para_init(&tmr_output_struct);
-    tmr_output_struct.oc_mode = TMR_OUTPUT_CONTROL_SWITCH;
-    tmr_output_struct.oc_polarity = TMR_OUTPUT_ACTIVE_LOW;
-    tmr_output_struct.oc_output_state = TRUE;
-    tmr_output_channel_config(TMR4, TMR_SELECT_CHANNEL_3, &tmr_output_struct);
-    tmr_channel_value_set(TMR4, TMR_SELECT_CHANNEL_3, TMR4_PERIOD_CLOCK);
-    tmr_counter_enable(TMR4, TRUE);
-
-    // Configure PB14 as timer 1 channel 2 output
-    gpio_default_para_init(&gpio_init_struct);
-    gpio_init_struct.gpio_pins = GPIO_PINS_14;
-    gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
-    gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
-    gpio_init_struct.gpio_mode = GPIO_MODE_MUX;
-    gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
-    gpio_init(GPIOB, &gpio_init_struct);
-    gpio_pin_mux_config(GPIOB, GPIO_PINS_SOURCE14, GPIO_MUX_1);
-
-    tmr_base_init(TMR1, TMR1_PERIOD_CLOCK, TMR1_FREQ_DIV);
-    tmr_cnt_dir_set(TMR1, TMR_COUNT_UP);
-    tmr_clock_source_div_set(TMR1, TMR_CLOCK_DIV1);
-
-    tmr_overflow_event_disable(TMR1, TRUE);
-    tmr_output_default_para_init(&tmr_output_struct);
-    tmr_output_struct.oc_mode = TMR_OUTPUT_CONTROL_SWITCH;
-    tmr_output_struct.occ_polarity = TMR_OUTPUT_ACTIVE_LOW;
-    tmr_output_struct.occ_output_state = TRUE;
-    tmr_output_channel_config(TMR1, TMR_SELECT_CHANNEL_2, &tmr_output_struct);
-    tmr_channel_value_set(TMR1, TMR_SELECT_CHANNEL_2, TMR1_PERIOD_CLOCK);
-    tmr_output_enable(TMR1, TRUE);
-    tmr_counter_enable(TMR1, TRUE);
-
-    // Enable interrupts
-    tmr_interrupt_enable(TMR4, TMR_C4_INT, TRUE);
-    tmr_interrupt_enable(TMR1, TMR_C4_INT, TRUE);
-
     vcom_console_init();
 
+    leds_blink(&leds, LED_AMBER);
+    leds_on(&leds, LED_GREEN);
+
     while (1) {
-        // Software LED2 toggle
-        leds_toggle(&leds, LED_RED);
-        delay_ms(100);
         vcom_console_run();
     }
 
     return 0;
-}
-
-void TMR1_CH_IRQHandler(void) {
-    if (tmr_flag_get(TMR1, TMR_C4_FLAG) != RESET) {
-        tmr_flag_clear(TMR1, TMR_C4_FLAG);
-    }
-}
-
-void TMR4_GLOBAL_IRQHandler(void) {
-    if (tmr_flag_get(TMR4, TMR_C4_FLAG) != RESET) {
-        tmr_flag_clear(TMR4, TMR_C4_FLAG);
-        leds_toggle(&leds, LED_GREEN);
-    }
 }
