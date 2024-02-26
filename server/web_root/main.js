6b2d97f6935666ec85166ff531cd8f11d04435eb
Vasiliy Nerozin <vnerozin@gmail.com>
2024-02-26 23:09:30 +0300

    Add TODOs and format js code

--- server/web_root/main.js
+++ server/web_root/main.js
@@ -1,10 +1,12 @@
-function loadSettings() {
-    fetch('/api/info')
-        .then(r => r.json())
-        .then(json => {
-            main.appendChild(json)
-        })
-        .catch(err => console.log(err));
+// TODO: Add js/css/html code formatter https://github.com/prettier/prettier and https://prettier.io/docs/en/precommit.html
+
+function loadSensors() {
+  fetch("/api/info")
+    .then((r) => r.json())
+    .then((json) => {
+      main.appendChild(json);
+    })
+    .catch((err) => console.log(err));
 }
 
-window.onload = loadSettings;
+window.onload = loadSensors;
